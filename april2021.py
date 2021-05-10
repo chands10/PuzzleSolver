989e48498f2b5dfcafe9e46fc136d8aecfa44ea0
Salil Chandra <salilc33@gmail.com>
2021-05-10 18:35:02 -0400

    Changed merge parameters, added in more comments

--- april2021.py
+++ april2021.py
@@ -1,32 +1,36 @@
 import pprint
 from math import inf
 
+# helper function for merge
 # fill results array in with probabilities of side1 making it to the next round by considering each team in side1 playing each team in side2 using dynamic programming
 # pr[i, j] = pr[i, j - 1] * sum_{k \in possible teams can face in round j} [k / (i + k) * pr[k, j - 1]]
 def play(side1, side2, results):
     for team1, p1 in side1:
         newProb = 0
+        # sum_{k \in possible teams can face in round j} [k / (i + k) * pr[k, j - 1]]
         for team2, p2 in side2:
             newProb += team2 / (team1 + team2) * p2
         
-        newProb *= p1
+        newProb *= p1 # multiply by pr[i, j - 1]
         results.append((team1, newProb))
+        
 
 # helper function for findProbWinning
-# teams in probs[idx] will be playing teams in probs[idx + 1]
+# sides are lists of teams who would meet in the next round of the bracket
+# each team in the list has played each other team in the same list
 # return probabilities after these teams play each other
-def merge(probs, idx):
-    side1 = probs[idx] # teams on one side of bracket that have already played each other
-    side2 = probs[idx + 1] # teams on other side of bracket that have already played each other
-    
+def merge(side1, side2):    
     results = []    
     play(side1, side2, results) # update probabilities for each team in side1 after playing each team in side2
     play(side2, side1, results) # update probabilities for each team in side2 after playing each team in side1
     
     return results
 
+
 """
-Dynamic programming algorithm
+Requires a bracket that has a length = n which is a power of 2
+Return a dictionary that contains the probability of a team (key) winning in its value
+Dynamic programming/divide and conquer algorithm
 
 Subproblem: pr[i, j] = probability that team i wins in round j
 
@@ -34,13 +38,17 @@ Reucrrence:
 Base Case: pr[i, 0] = 1
 pr[i, j] = pr[i, j - 1] * sum_{k \in possible teams can face in round j} [k / (i + k) * pr[k, j - 1]]
 
+Return pr[team, log_2(n)]
+
 The probability that team i wins in round j is the probability that team i makes it to round j (pr[i, j - 1]) multiplied by the probability that team i wins against their opponent in round j (sum_{k} [...])
 
 The probability that team i wins against their opponent in round j is the sum over k of the probability that team i beats team k (which is k / (i + k)) multiplied by the probability that team k makes it to round j (pr[k, j - 1])
 
 This algorithm will iterate through one round at a time, reducing the need for a 2 dimensional array.
 
-Requires a bracket that is a power of 2 in length
+The number of possible teams a team can face in round j is 2^(j - 1) < n. This is also a divide and conquer problem.
+
+By the Master Theorem we have that we are solving a = 2 subproblems of size n / b = n / 2 and then combining these answers in O(n^d) = O(n^2) time. Thus a = b = d = 2 and 2 > log_2(2) so the running time of this function is O(n^2) 
 """ 
 def findProbWinning(bracket):
     # probs will be a list of lists of tuples. The tuples represents a team and its probability of making it to that round. The most inner list contains all of the teams that have already faced each other. Lists here that are adjacent to each other (in pairs of 2s) will face each other in the next round
@@ -48,7 +56,7 @@ def findProbWinning(bracket):
     while len(probs) > 1: # log_2(n) iterations.
         newProbs = [0] * (len(probs) // 2)
         for j in range(0, len(probs), 2): # merge like in merge sort
-            newProbs[j // 2] = merge(probs, j)
+            newProbs[j // 2] = merge(probs[j], probs[j + 1])
         
         probs = newProbs
     
@@ -58,8 +66,9 @@ def findProbWinning(bracket):
             
 
 if __name__ == "__main__":
+    # bracket must have length that is a power of 2
     bracket = [1, 16, 8, 9, 5, 12, 4, 13, 6, 11, 3, 14, 7, 10, 2, 15]
-    team = 2
+    team = 2 # team to find the best outcome of a swap for
     
     pp = pprint.PrettyPrinter()
     
